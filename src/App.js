f4ee29d20eac7e182de67425fffc2b4c85f00691
Lakshmi M S <lakshu.ms@gmail.com>
2023-02-22 14:17:50 -0500

    Search engine for word search

--- src/App.js
+++ src/App.js
@@ -1,16 +1,47 @@
 import logo from "./logo.jpeg";
 import React from "react";
 import "./App.css";
+import WordSearch from "./WordSearch";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo img-fluid" alt="logo" />
-        <strong>Dictionary</strong>
-      </header>
+      <div className="container">
+        <header className="App-header card">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+          <strong>Dictionary</strong>
+        </header>
+        <main>
+          <WordSearch />
+        </main>
+        <footer className="Footer card1">
+          Coded by{" "}
+          <a
+            href="https://www.linkedin.com/in/lakshmi-m-satyananda-3b726930"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Lakshmi M Satyananda <i className="fa-brands fa-linkedin"></i>
+          </a>
+          , Open-Sourced on{" "}
+          <a
+            href="https://github.com/laksh-ms"
+            target="_blank"
+            rel="noreferrer"
+          >
+            github <i className="fa-brands fa-github"></i>
+          </a>{" "}
+          and hosted on{" "}
+          <a
+            href="https://meek-madeleine-ad7b86.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Netlify
+          </a>
+          .
+        </footer>
+      </div>
     </div>
   );
 }
-
-export default App;
---
+++ src/WordSearch.js
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+
+export default function WordSearch() {
+  const [word, setWord] = useState("");
+
+  function search(event) {
+    event.preventDefault();
+    alert(`Searching for ${word}`);
+  }
+  function handleChange(event) {
+    setWord(event.target.value);
+  }
+  return (
+    <div className="WordSearch">
+      <form onSubmit={search}>
+        <input type="search" onChange={handleChange} />
+      </form>
+      <strong>Hello from WordSearch!</strong>
+    </div>
+  );
+}
