99a93c2a11b38a389d13a7db411dd8231cfaff3f
Lakshmi M S <lakshu.ms@gmail.com>
2023-02-22 19:32:56 -0500

    Api integration

--- src/WordSearch.js
+++ src/WordSearch.js
@@ -1,19 +1,25 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 export default function WordSearch() {
   const [word, setWord] = useState("");
 
+  function handleResponse(response) {}
+
   function search(event) {
     event.preventDefault();
-    alert(`Searching for ${word}`);
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
+    axios.get(apiUrl).then(handleResponse);
   }
+
   function handleChange(event) {
     setWord(event.target.value);
   }
+
   return (
     <div className="WordSearch">
       <form onSubmit={search}>
-        <input type="search" onChange={handleChange} />
+        <input type="search" className="form-control" onChange={handleChange} />
       </form>
     </div>
   );
